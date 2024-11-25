17e170d14f2f95769691c9e10b420964e3cddb0a
jaum537 <joaobarbosa5337@gmail.com>
2024-11-24 21:00:57 -0300

    Adição github actions

--- cypress/e2e/SweetShop.cy.js
+++ cypress/e2e/SweetShop.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Acessar e realizar login e manter a sessão', () => {
+  it('Falha no teste', () => {
+    cy.visit('https://sweetshop.netlify.app/')
+    cy.visit('https://sweetshop.netlify.app/login')
+    cy.get('#exampleInputEmail.form-control').clear().type('test@user.com')
+    cy.get('#exampleInputPassword.form-control').clear().type('qwerty')
+    cy.get('.btn.btn-primary').click()
+    cy.contains('Welcome back test@user.com').should('be.visible')
+    cy.visit('https://sweetshop.netlify.app/login')
+    cy.contains('Welcome back test@user.com').should('be.visible')
+  })
+})
+
+describe('', () => {
+  it('', () => {
+    
   })
 })
